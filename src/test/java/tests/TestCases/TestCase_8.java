6b5af1e9ae4d7f28fc861da2e0a7f5d63ae82c36
wagih-essam <wageh_essam2010@yahoo.com>
2025-02-14 14:59:38 +0200

    editing test case eight

--- src/test/java/tests/TestCases/TestCase_8.java
+++ src/test/java/tests/TestCases/TestCase_8.java
@@ -20,14 +20,5 @@ public class TestCase_8 extends BaseTest {
                 .validateProductsList(ProductsPage.validProductsElements)
                 .clickOnFirstProduct().verifyProductImageIsVisible().verifyFirstProductDetails();
     }
-    @Test
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Test Case Eight-2")
-    public void verifyInvalidAllProductsAndProductDetailPage(){
-        HomePage homePage = new HomePage(driver);
-        homePage.navigateToAutomationHomePage().verifyHomePageVisibility()
-                .goToProductsPage().verifyAllProductsTitleIsVisible()
-                .validateProductsList(ProductsPage.inValidProductsElements);
-    }
 
 }
